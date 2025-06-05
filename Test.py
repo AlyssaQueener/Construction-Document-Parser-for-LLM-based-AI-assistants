a8514346dbc6b05f66e9dc0f986d27a41b56f39d
Bekkare <becks.buter@gmail.com>
2025-06-05 16:12:28 +0200

    boq4 change

--- Test.py
+++ Test.py
@@ -1,11 +1,29 @@
+#from src.boq2data import extract_data
+#from src.boq2data.extract.loader import read_templates
+#from src.boq2data.output.format_boq import format_boq
+#import json
+
+#filename = "examples/FinancialDocuments/BoQ4.pdf"
+#templates = read_templates('examples/FinancialDocuments/templates')
+#result = extract_data(filename, templates=templates)
+#structured_result = format_boq(result)
+#with open('test.json', 'w', encoding='utf-8') as f:
+ #   json.dump(structured_result, f, indent=4, ensure_ascii=False)
+
 from src.boq2data import extract_data
 from src.boq2data.extract.loader import read_templates
-from src.boq2data.output.format_boq import format_boq
+from src.boq2data.output import to_json
 import json
 
 filename = "examples/FinancialDocuments/BoQ4.pdf"
 templates = read_templates('examples/FinancialDocuments/templates')
 result = extract_data(filename, templates=templates)
-structured_result = format_boq(result)
+
+# Print and write result
+print("result")
+print(json.dumps(result, indent=4, ensure_ascii=False))  # Pretty print
+
+# Save to JSON file
 with open('test.json', 'w', encoding='utf-8') as f:
-    json.dump(structured_result, f, indent=4, ensure_ascii=False)
+    json.dump(result, f, indent=4, ensure_ascii=False)
+    print(" JSON saved to test.json")
