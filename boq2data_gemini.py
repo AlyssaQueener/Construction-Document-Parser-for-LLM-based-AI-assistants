6b97045d8e84fcbfd0f3b0f31144d6af4e7f3a5b
AlyssaQueener <alyssamayqueener@gmail.com>
2025-07-11 12:37:34 +0200

    Update boq2data_gemini.py

--- boq2data_gemini.py
+++ boq2data_gemini.py
@@ -2,35 +2,54 @@ import src.boq2data.camelot_setup.Camelot_Functions as cam
 import json
 import camelot
 import src.boq2data.camelot_setup.gemini as gemini 
+import matplotlib
 import csv
 import asyncio 
 
+
+def financial_boq(path):
+        page_num = "all"
+        flav = "stream"
+        tables = camelot.read_pdf(path, flavor=flav, pages=page_num) # -> output camelot table object 
+        tables_boq_processed = cam.cam_stream_merge(tables) # json 
+        processed_str = json.dumps(tables_boq_processed, indent=2, ensure_ascii=False)
+        prompt = gemini.create_preproccesed_prompt(processed_str)
+        response_json = gemini.call_gemini_return_json(prompt)
+        return response_json
+       
+
+
+
 if __name__ == "__main__":
+    
+    
     path = 'examples/FinancialDocuments/Bill-of-Quantities-Sample2.pdf'
-    flav = 'network'
-    page_num = '1'
+    flav = 'stream'
+    page_num = 'all'
     #tables_boq4 = cam.cam_extract(path,flav,page_num)
    
    # unprocessed and processed values 
-    tables = camelot.read_pdf(path, flavor=flav, pages=page_num)
-    tables_boq_processed = cam.cam_stream_merge(tables)
+    tables = camelot.read_pdf(path, flavor=flav, pages=page_num) # type: ignore # -> output camelot table object 
+    #camelot.plot(tables[0], kind='text').show()
     
-    # Display tables in json and csv 
+    tables_boq_processed = cam.cam_stream_merge(tables) # json 
+    
+###### # Display tables in json and csv 
     print(tables)
 #     tables.export('foo.csv',f='csv')
 
     print(tables_boq_processed)
-#     fieldnames = sorted(tables_boq_processed[0].keys()) if tables_boq_processed else []
-#     with open('output.csv', 'w', encoding='utf-8', newline='') as f:
-#         writer = csv.DictWriter(f, fieldnames=fieldnames)
-#         writer.writeheader()
-#         writer.writerows(tables_boq_processed)
+    fieldnames = sorted(tables_boq_processed[0].keys()) if tables_boq_processed else []
+    with open('output.csv', 'w', encoding='utf-8', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(tables_boq_processed)
 
-# Prepare processed input for gemini 
-# stringify the json again 
+#Prepare processed input for gemini 
+#stringify the json again 
     processed_str = json.dumps(tables_boq_processed, indent=2, ensure_ascii=False)
     #Step 1: Generate the prompt using your function
-    prompt = gemini.create_preproccesed_prompt(tables_boq_processed)
+    prompt = gemini.create_preproccesed_prompt(processed_str)
     print("---- PROMPT SENT TO GEMINI ----")
     print(prompt)
     print("---- END OF PROMPT ----")
