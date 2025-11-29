63c2dadd6bd729d7a433dbdd3e5e0ed5d1ece069
AlyssaQueener <alyssamayqueener@gmail.com>
2025-11-29 13:58:52 +0100

    wip

--- examples/test-visual-2/auto/test-visual-2.md
+++
@@ -1,3 +0,0 @@
-# Parking Lot Paving
-
-![](images/9c695e9d0c34814f7bff31b71704144789876ef13e734ca6dfa0dae787e41608.jpg)
--- testGantt.py
+++
@@ -1,9 +0,0 @@
-import src.gantt2data.mineru as mineru
-
-
-
-path = ["src/validation/Gantt/testdata/test-visual-2.pdf"]
-output ="examples"
-
-mineru.parse_doc(path,output,"en")
-
