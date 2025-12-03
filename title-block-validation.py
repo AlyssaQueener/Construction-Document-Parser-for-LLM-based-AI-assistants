37d9f31333419fc9c6b23d1b32b07c8c6dc95cee
AlyssaQueener <alyssamayqueener@gmail.com>
2025-12-03 17:13:06 +0100

    wip

--- title-block-validation.py
+++ title-block-validation.py
@@ -1,31 +1,13 @@
 import src.plan2data.titleBlockInfo as parser
 import src.validation.Floorplan.titleblock.llm_as_a_judge as llm_judge
+import src.plan2data.helper as helper
+
+path1 = "src/validation/Floorplan/neighboring rooms/Cluttered Floorplans/Cluttered 01.pdf"
+path2 = "src/validation/Floorplan/neighboring rooms/Cluttered Floorplans/Cluttered 02.pdf"
+path3 = "src/validation/Floorplan/neighboring rooms/Cluttered Floorplans/Cluttered 03.pdf"
 
 
-image_path = "src/validation/Floorplan/titleblock/testdata/floorplan-test_page1.png"
-image_path_1 = "src/validation/Floorplan/titleblock/testdata/floorplan-test-1_page1.png"
-image_path_2 = "src/validation/Floorplan/titleblock/testdata/floorplan-test-2_page1.png"
 
-validation_path = "src/validation/Floorplan/titleblock/testdata/floorplan-test.json"
-validation_path_1 = "src/validation/Floorplan/titleblock/testdata/floorplan-test-1.json"
-validation_path_2 = "src/validation/Floorplan/titleblock/testdata/floorplan-test-2.json"
-
-#result = parser.get_title_block_info_ai(image_path)
-result_1 = parser.get_title_block_info(image_path_1)
-#result_2 = parser.get_title_block_info_ai(image_path_2)
-
-print("Result of first example")
-#print(result)
-print("Result of second example")
-print(result_1)
-print("Result of third example")
-#print(result_2)
-
-print("Validation Results:")
-#print("1. Example:")
-#print(llm_judge.llm_as_a_judge_titleblock(validation_path, result))
-#print("2. Example:")
-#print(llm_judge.llm_as_a_judge_titleblock(validation_path_1, result_1))
-#print("3. Example:")
-print(llm_judge.llm_as_a_judge_titleblock(validation_path_1, result_1))
-
+image1 = helper.convert_pdf2img(path1)
+image2 = helper.convert_pdf2img(path2)
+image3 = helper.convert_pdf2img(path3)
